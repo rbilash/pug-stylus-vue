a84ba7006c0a06d7a2770e4634f234c30104aeaf
Ruslan Bilash <bilash@gmail.com>
2018-06-21 11:04:08 +0300

    init

---
+++ src/components/AppComponent.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div 
+    p Hello {{ who }}
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                who: "World"
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+  div
+    p
+      color: red
+</style>
